da9c308d6c8d185005b4557b67b25d6407269358
Federico Raimondi <federico.raimondi9@gmail.com>
2021-12-02 11:18:59 +0100

    Add bottom bar

---
+++ dressandgo/client/src/mycomponents/bottombar.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
+import Home from '@mui/icons-material/Home';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+
+
+
+
+export default function FixedBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef(null);
+
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+    
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Home" icon={<Home />} />
+          <BottomNavigationAction label="FAQ" icon={<QuestionMarkIcon />} />
+          <BottomNavigationAction label="My rents" icon={<FormatListBulletedIcon />} />
+          <BottomNavigationAction label="Account" icon={<AccountCircleIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
+
