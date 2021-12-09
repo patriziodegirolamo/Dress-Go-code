857982ef96e8a2cea395657b7f2558d471773d10
federai9 <federico.raimondi9@gmail.com>
2021-12-09 11:08:20 +0100

    update profile

--- dressandgo/client/src/mycomponents/profile.js
+++ dressandgo/client/src/mycomponents/profile.js
@@ -9,8 +9,8 @@ function MyProfile(props) {
     
             <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
             <div class="card p-4">
-            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" /></button> <span class="name mt-3">Eleanor Pena</span> <span class="idd">@eleanorpena</span>
-            <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
+            <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" /></button> <span class="name mt-3">Andrea Birdwhistle</span> <span class="idd">@andrew19</span>
+            <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Following</span></span> </div>
             <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div>
             <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork. Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div>
             <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i class="fa fa-twitter"></i></span> <span><i class="fa fa-facebook-f"></i></span> <span><i class="fa fa-instagram"></i></span> <span><i class="fa fa-linkedin"></i></span> </div>
